b47a2d5a89ed68775eb4b8cd6b4acb431f2517b5
dangngockhieu <dangngockhieu28082005@gmail.com>
2025-11-08 20:22:52 +0700

    first commit

---
+++ src/main/java/vn/techzone/khieu/KhieuApplication.java
@@ -0,0 +1,13 @@
+package vn.techzone.khieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhieuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/techzone/khieu/KhieuApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.techzone.khieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhieuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
