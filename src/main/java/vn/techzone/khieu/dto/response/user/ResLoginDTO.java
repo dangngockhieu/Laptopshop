2c4a894a807a3e77e327616cd2efa5343c9b67f6
dangngockhieu <dangngockhieu28082005@gmail.com>
2025-11-16 11:43:32 +0700

    hoàn thiện api login

--- src/main/java/vn/techzone/khieu/dto/response/user/ResLoginDTO.java
+++ src/main/java/vn/techzone/khieu/dto/response/user/ResLoginDTO.java
@@ -1,10 +1,26 @@
 package vn.techzone.khieu.dto.response.user;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResLoginDTO {
     private String access_token;
+    private UserInfo user;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserInfo {
+        private long id;
+        private String name;
+        private String email;
+        private String role;
+    }
 }
--- src/main/java/vn/techzone/khieu/service/user/UserDetailsCustom.java
+++ src/main/java/vn/techzone/khieu/service/user/UserDetailsCustom.java
@@ -1,10 +1,12 @@
-package vn.techzone.khieu.service;
+package vn.techzone.khieu.service.user;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import vn.techzone.khieu.entity.User;
+import vn.techzone.khieu.service.UserService;
 
 @Component("userDetailsService")
 public class UserDetailsCustom implements UserDetailsService {
@@ -16,14 +18,15 @@ public class UserDetailsCustom implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        vn.techzone.khieu.entity.User user = this.userService.findUserByEmail(email)
+        User user = this.userService.findUserByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-        return User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .roles(user.getRole())
-                .disabled(!user.getVerified())
-                .build();
+        return new UserPrincipal(
+                user.getId(),
+                user.getEmail(),
+                user.getName(),
+                user.getRole(),
+                user.getPassword(),
+                user.getVerified());
     }
 
 }
