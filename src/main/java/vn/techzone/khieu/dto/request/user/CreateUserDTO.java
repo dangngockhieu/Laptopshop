9c4d045c32af34f4f3d5fcb70cb522f4a49df6c7
dangngockhieu <dangngockhieu28082005@gmail.com>
2026-03-12 09:38:37 +0700

    hoàn thiện UserController

--- src/main/java/vn/techzone/khieu/dto/request/user/CreateUserDTO.java
+++ src/main/java/vn/techzone/khieu/dto/request/user/CreateUserDTO.java
@@ -3,6 +3,8 @@ package vn.techzone.khieu.dto.request.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -15,6 +17,8 @@ public class CreateUserDTO {
     private String email;
 
     @NotBlank(message = "Password cannot be empty")
+    @Size(min = 8, max = 32, message = "Password must be 8-32 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$", message = "Password must contain uppercase, lowercase, number, special character")
     private String password;
 
     @NotBlank(message = "Role cannot be empty")
--- src/main/java/vn/techzone/khieu/service/UserService.java
+++ src/main/java/vn/techzone/khieu/service/UserService.java
@@ -6,11 +6,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import vn.techzone.khieu.dto.request.user.CreateUserDTO;
-import vn.techzone.khieu.dto.request.user.UpdateUserDTO;
+import vn.techzone.khieu.dto.request.user.UpdatePasswordDTO;
 import vn.techzone.khieu.dto.response.PageResponseDTO;
 import vn.techzone.khieu.dto.response.user.ResUserDTO;
 import vn.techzone.khieu.entity.User;
@@ -24,7 +25,9 @@ import vn.techzone.khieu.utils.error.NotFoundUserException;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
+    // UserController
     public boolean isEmailExist(String email) {
         return this.userRepository.existsByEmail(email);
     }
@@ -53,17 +56,38 @@ public class UserService {
         return this.userMapper.toResUserDTO(user);
     }
 
-    public ResUserDTO handleCreateUser(CreateUserDTO userDTO) {
+    public ResUserDTO handleCreateUserForAdmin(CreateUserDTO userDTO) {
+        String hashPassword = passwordEncoder.encode(userDTO.getPassword());
+        userDTO.setPassword(hashPassword);
         User user = userMapper.toUser(userDTO);
         user.setVerified(true);
         User savedUser = this.userRepository.save(user);
         return this.userMapper.toResUserDTO(savedUser);
     }
 
-    public ResUserDTO handleUpdateUser(long id, UpdateUserDTO updateUserDTO) {
+    public ResUserDTO handleUpdatePassword(String email, UpdatePasswordDTO updatePasswordDTO) {
+        if (!updatePasswordDTO.getNewPassword().equals(updatePasswordDTO.getConfirmPassword())) {
+            throw new IllegalArgumentException("Passwords do not match");
+        }
+        User existingUser = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundUserException("Không tìm thấy User với ID: " + email));
+        boolean isMatch = passwordEncoder.matches(updatePasswordDTO.getOldPassword(), existingUser.getPassword());
+        if (!isMatch) {
+            throw new IllegalArgumentException("Mật khẩu không chính xác");
+        }
+        String hashPassword = passwordEncoder.encode(updatePasswordDTO.getNewPassword());
+        existingUser.setPassword(hashPassword);
+        User user = this.userRepository.save(existingUser);
+        return this.userMapper.toResUserDTO(user);
+    }
+
+    public ResUserDTO handleUpdateRole(long id, String role) {
         User existingUser = this.userRepository.findById(id)
                 .orElseThrow(() -> new NotFoundUserException("Không tìm thấy User với ID: " + id));
-        userMapper.updateUserFromDTO(updateUserDTO, existingUser);
+        if (!role.equals("USER") && !role.equals("ADMIN")) {
+            throw new IllegalArgumentException("Vai trò không hợp lệ");
+        }
+        existingUser.setRole(role);
         User user = this.userRepository.save(existingUser);
         return this.userMapper.toResUserDTO(user);
     }
@@ -72,6 +96,11 @@ public class UserService {
         this.userRepository.deleteById(id);
     }
 
+    public long countUsers() {
+        return this.userRepository.countByIsVerifiedTrue();
+    }
+
+    // AuthController
     public Optional<User> findByEmailAndRefreshToken(String email, String refreshToken) {
         String hashed = SecurityUtil.hashWithSHA256(refreshToken);
         return this.userRepository.findByEmailAndRefreshToken(email, hashed);
@@ -87,6 +116,15 @@ public class UserService {
             user.setRefreshToken(hashedToken);
         }
         this.userRepository.save(user);
+    }
+
+    public ResUserDTO handleRegister(CreateUserDTO userDTO) {
+        String hashPassword = passwordEncoder.encode(userDTO.getPassword());
+        userDTO.setPassword(hashPassword);
+        User user = userMapper.toUser(userDTO);
+        user.setVerified(false);
+        User savedUser = this.userRepository.save(user);
+        return this.userMapper.toResUserDTO(savedUser);
     }
 
 }
